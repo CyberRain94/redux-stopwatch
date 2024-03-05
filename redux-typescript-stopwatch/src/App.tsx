3b9301d51a1e34c9528df3b6121973adb9e969ae
CyberRain94 <bourdierkelvin@gmail.com>
2024-03-05 08:55:28 +0000

    Created App Component/Added Draggable package

--- redux-typescript-stopwatch/src/App.tsx
+++ redux-typescript-stopwatch/src/App.tsx
@@ -1,58 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Stopwatch';
-import './App.css';
-
-function App() {
+import { Provider } from 'react-redux';
+import store from "./app/store";
+import Stopwatch from './features/counter/Stopwatch';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import Draggable from "react-draggable";
+  const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <Provider store={store}>
+        <Draggable>
+        <div id="StopWatch" >
+      <div id="CheckInTimer" className="container pt-5 jumbotron">
+      <div id="quote-box" className="container text-center card">
+        <div className="card-body">
+          <Stopwatch />
+        </div>
+      </div>
+      <footer id='footer' className="text-center">
+        Coded by:
+        <a href={"https://github.com/CyberRain94"} target="_blank">
+          CyberRain94
+        </a>
+      </footer>
     </div>
-  );
-}
+    </div>
+    </Draggable>
+    </Provider>
+  )
+};
 
 export default App;
