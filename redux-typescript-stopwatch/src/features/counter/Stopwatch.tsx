2466bfde4b3c7ef9f174e5bfb2e2f6cf1b9ff7bd
Cyberrain94 <bourdierkelvin@gmail.com>
2024-07-31 20:02:13 -0400

    Created formaing for hours / env file

--- redux-typescript-stopwatch/src/features/counter/Stopwatch.tsx
+++ redux-typescript-stopwatch/src/features/counter/Stopwatch.tsx
@@ -22,9 +22,10 @@ const Stopwatch: React.FC = () => {
     };
   }, [isRunning, dispatch]);
   const formatTime = (seconds: number): string => {
-    const minutes = Math.floor(seconds / 60);
+    const hours = (seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
   };
   return (
     <div>
