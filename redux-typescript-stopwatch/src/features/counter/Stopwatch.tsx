9738b41a152439002a9ce392e0d4efef905bd9d4
Cyberrain94 <bourdierkelvin@gmail.com>
2024-07-31 23:29:43 -0400

    Fix stopwatch issue

--- redux-typescript-stopwatch/src/features/counter/Stopwatch.tsx
+++ redux-typescript-stopwatch/src/features/counter/Stopwatch.tsx
@@ -22,11 +22,12 @@ const Stopwatch: React.FC = () => {
     };
   }, [isRunning, dispatch]);
   const formatTime = (seconds: number): string => {
-    const hours = (seconds / 3600)
+    const hours = Math.floor(seconds / 3600)
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
     return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
   };
+
   return (
     <div>
       <span className={styles.value}>Time {formatTime(elapsedTime)}</span>
